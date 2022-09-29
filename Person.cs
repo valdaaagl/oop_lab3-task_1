2eca376529389867dc8d9d782368726716a59684
vlada <vsh_o@ukr.net>
2022-09-29 12:39:43 +0300

    First task of the third lab

---
+++ Person.cs
@@ -0,0 +1,8 @@
+public class Person
+{
+    public void Person_1(string name, string surname, int age)
+    {
+        
+        Console.WriteLine($"Name: {name} {surname}, age: {age}");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Person person = new Person();
+person.Person_1("Vlada", "Hlushchenko", 18);
+
+Person person_1 = new Employee();
+person_1.Person_1("Dima", "O.", 17);
